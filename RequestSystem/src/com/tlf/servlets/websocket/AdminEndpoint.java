233a80cfd1590ce3a087484df452863276519ba8
thislooksfun <thislooksfun@repbot.org>
2014-03-30 22:05:12 -0500

    Created AdminEndpoint to deal with admin messages

---
+++ RequestSystem/src/com/tlf/servlets/websocket/AdminEndpoint.java
@@ -0,0 +1,57 @@
+package com.tlf.servlets.websocket;
+
+import java.io.IOException;
+
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import com.tlf.itunes.LibraryDecoder;
+import com.tlf.itunes.Song;
+import com.tlf.servlets.websocket.encoders.SongEncoder;
+import com.tlf.util.WebsocketHelper;
+
+/**
+ * Servlet implementation class AdminEndpoint
+ */
+@ServerEndpoint(value = "/websocket/admin",
+encoders = {SongEncoder.class})
+public class AdminEndpoint
+{
+	@OnOpen
+	public void onOpen(Session session) {
+		WebsocketHelper.openSession(session);
+	}
+	
+	@OnClose
+	public void onClose(Session session, CloseReason reason) {
+		WebsocketHelper.closeSession(session, reason);
+	}
+	
+	@OnMessage
+	public void onMessage(Session session, String msg)
+	{
+		try {
+			if (msg.indexOf("PLAYED:") == 0) {
+				System.out.println("Request!");
+				Song song = LibraryDecoder.getSong(Integer.parseInt(msg.substring(7)));
+				song.requests = 0;
+				WebsocketHelper.sendRequestUpdate(song.trackID(), song.requests);
+			}
+			session.getBasicRemote().sendText(msg);
+		} catch (IOException | NumberFormatException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@OnError
+	public void error(Session session, Throwable throwable)
+	{
+		System.err.println("ERROR:");
+		throwable.printStackTrace();
+	}
+}
